7b2122a624b2c56cbf70cd2989a28fe8a4b3efdd
Jaden Hums <jadenhums51@gmail.com>
2026-01-28 15:59:21 -0800

    2025 day 2 rust part 1 iteration 1

---
+++ aoc-2025-rust/src/giftshop.rs
@@ -0,0 +1,31 @@
+use std::fs;
+
+pub fn giftshop(inputfile: &str) -> u64 {
+    println!("Checking gift shop invalid product ids");
+
+    let content = fs::read_to_string(inputfile).expect("Error reading file");
+    let ids = content.trim().split(",");
+
+    let mut result: u64 = 0;
+
+    for id_range in ids {
+        let mut bounds = id_range.split("-");
+        let lower_bound: u64 = bounds.next().unwrap().trim().parse::<u64>().unwrap();
+        let upper_bound: u64 = bounds.next().unwrap().trim().parse::<u64>().unwrap();
+
+        for id in lower_bound..(upper_bound + 1) {
+            let id_string = id.to_string();
+            if id_string.len() % 2 == 1 {
+                continue;
+            }
+
+            let mid = id_string.len() / 2;
+            let (left, right) = id_string.split_at(mid);
+            if left == right {
+                result += id
+            }
+        }
+    }
+
+    result
+}
--- aoc-2025-rust/src/main.rs
+++ aoc-2025-rust/src/main.rs
@@ -1,8 +1,26 @@
-pub mod secret_entrance;
+mod giftshop;
+mod secret_entrance;
 
 fn main() {
-    println!(
-        "The secret entrance code is {}!",
-        secret_entrance::secret_entrance("inputs/day1.txt")
-    );
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Usage: {} <day>", args.first().unwrap());
+        std::process::exit(1);
+    }
+
+    match args[1].as_str() {
+        "1" => {
+            let code = secret_entrance::secret_entrance("inputs/day1.txt");
+            println!("The secret entrance code is {}!", code);
+        }
+        "2" => {
+            let invalid_id_sum = giftshop::giftshop("inputs/day2.txt");
+            println!("The sum of invalid ids is {}!", invalid_id_sum);
+        }
+        _ => {
+            eprintln!("Day not found {}", args[1]);
+            std::process::exit(1);
+        }
+    }
 }
