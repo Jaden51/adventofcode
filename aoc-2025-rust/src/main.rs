1477c0319e6a505e6200bdc8def7d226a66672cb
Jaden Hums <jadenhums51@gmail.com>
2026-01-27 16:47:55 -0800

    2025 day 1 rust code review

--- aoc-2025-rust/src/main.rs
+++ aoc-2025-rust/src/main.rs
@@ -2,7 +2,7 @@ pub mod secret_entrance;
 
 fn main() {
     println!(
-        "The secret entrance code is {}",
+        "The secret entrance code is {}!",
         secret_entrance::secret_entrance("inputs/day1.txt")
     );
 }
--- aoc-2025-rust/src/secret_entrance.rs
+++ aoc-2025-rust/src/secret_entrance.rs
@@ -2,30 +2,20 @@ use std::fs;
 
 pub fn secret_entrance(filepath: &str) -> i32 {
     println!("Unlocking Secrent Entrance!");
-    println!("{filepath}");
-    let contents = fs::read_to_string(filepath).expect("Error reading file");
-    let lines: Vec<String> = contents.lines().map(|line| line.to_string()).collect();
+    let content = fs::read_to_string(filepath).expect("Error reading file");
 
     let mut current: i32 = 50;
     let mut result: i32 = 0;
 
-    for step in lines {
-        let direction = &step[..1];
-        let movement = &step[1..];
-        let mut mov_int: i32 = movement.parse().expect("Not a number");
-        mov_int %= 100;
+    for step in content.lines() {
+        let direction = step.chars().next().expect("Empty line");
+        let movement = &step[1..].parse().expect("Not a number");
 
-        if direction == "L" {
-            current -= mov_int;
-            if current < 0 {
-                current += 100;
-            }
-        } else if direction == "R" {
-            current += mov_int;
-            if current > 99 {
-                current -= 100;
-            }
-        }
+        current = match direction {
+            'L' => (current - movement).rem_euclid(100),
+            'R' => (current + movement).rem_euclid(100),
+            _ => panic!("Unknown direction: {direction}"),
+        };
 
         if current == 0 {
             result += 1
