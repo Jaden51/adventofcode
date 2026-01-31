489db94c3dc3fbd6192bd90cd32bf16a9956ebbf
Jaden Hums <jadenhums51@gmail.com>
2026-01-31 11:30:31 -0800

    2025 day 4 rust part 2 iteration 1

--- aoc-2025-rust/src/printing_department.rs
+++ aoc-2025-rust/src/printing_department.rs
@@ -1,5 +1,6 @@
 use std::fs;
 
+const OPEN_SPACE: char = '.';
 const ROLL: char = '@';
 const DIRECTIONS: [[isize; 2]; 8] = [
     [-1, -1],
@@ -16,15 +17,28 @@ pub fn printing_department(inputfile: &str) -> usize {
     println!("Optimizing forklifts!");
 
     let contents = fs::read_to_string(inputfile).expect("Error reading file");
-    let map: Vec<Vec<char>> = contents
+    let mut result: usize = 0;
+    let mut map: Vec<Vec<char>> = contents
         .lines()
         .map(|line| line.chars().collect())
         .collect();
+    loop {
+        let (rolls_removed, new_map) = remove_rolls(map);
+        if rolls_removed == 0 {
+            break;
+        }
 
+        result += rolls_removed;
+        map = new_map;
+    }
+    result
+}
+
+pub fn remove_rolls(mut map: Vec<Vec<char>>) -> (usize, Vec<Vec<char>>) {
     let rows = map.len();
     let cols = map[0].len();
-
-    let mut result: usize = 0;
+    let mut rolls_removed: usize = 0;
+    let mut to_remove: Vec<(usize, usize)> = Vec::new();
 
     for r in 0..rows {
         for c in 0..cols {
@@ -50,10 +64,14 @@ pub fn printing_department(inputfile: &str) -> usize {
                 }
 
                 if roll_count < 4 {
-                    result += 1;
+                    rolls_removed += 1;
+                    to_remove.push((r, c));
                 }
             }
         }
     }
-    result
+    for (r, c) in to_remove {
+        map[r][c] = OPEN_SPACE;
+    }
+    (rolls_removed, map)
 }
